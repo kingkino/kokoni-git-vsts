83b746df8254aec0cb45f530319f86c923a362a6
DESKTOP-P9U9NMP\kingk <kingkino@gmail.com>
2017-10-01 19:51:58 +0900

    NewtonSoftの最新版をNugetについか

--- kokoin_library_framework/kokoin_library_framework/calcurato.cs
+++ kokoin_library_framework/kokoin_library_framework/calcurato.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace kokoin_library_framework
 {
-    public class Class1
+    public class calcurator
     {
+        public long add (long a,long b)
+        {
+            return a + b;
+        }
+
+        public long sub(long a, long b)
+        {
+            return a - b;
+        }
+
+        public long multi (long a, long b)
+        {
+            return a * b;
+        }
+
+        public long div(long a, long b)
+        {
+            if (a == 0 || b == 0) return 0;
+            return a / b;
+        }
     }
 }
