b61565cfc978280499bfdf17dc14a5bef6f0ec4e
DESKTOP-P9U9NMP\kingk <kingkino@gmail.com>
2017-10-01 19:36:08 +0900

    初期コミット

---
+++ kokoin_library_framework/kokoin_library_framework/calcurato.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kokoin_library_framework
+{
+    public class Class1
+    {
+    }
+}
